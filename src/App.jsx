4cc5b4c306b1f45da17d778cbb92586e158baa7d
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-17 10:04:39 +0530

    Initialize React App Using Vitejs

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello Vite + React + Tailwindcss + Eslint</p>
+    </div>
+  );
+}
+
+export default App;
