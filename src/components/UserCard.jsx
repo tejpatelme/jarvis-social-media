2e927f1fc94295dc6f184292594517ed19f7750e
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-27 21:42:23 +0530

    feat : only show follow button if not current logged in user

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { GenericAvatar, FollowUserButton } from "./";
 
 export default function UserCard({ user }) {
+  const { currentUser } = useSelector((state) => state.auth);
   return (
     <div className="p-4 rounded ring-1 ring-purple-200 ring-opacity-20 flex justify-between items-center w-full">
       <div className="flex">
@@ -20,7 +22,7 @@ export default function UserCard({ user }) {
           </span>
         </div>
       </div>
-      <FollowUserButton userId={user._id} />
+      {currentUser._id !== user._id && <FollowUserButton userId={user._id} />}
     </div>
   );
 }
