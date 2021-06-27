5fa08971bb2f884c2ec0e73c5fd7b3911d6e4fed
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-27 18:12:31 +0530

    re-exports post components

---
+++ src/features/posts/components/index.jsx
@@ -0,0 +1,7 @@
+export { default as CreatePost } from "./CreatePost";
+export { default as FormatISOString } from "./FormatISOString";
+export { default as PostCard } from "./PostCard";
+export { default as TimeAgo } from "./TimeAgo";
+export { default as CreateComment } from "./CreateComment";
+export { default as CommentCard } from "./CommentCard";
+export { default as CommentsContainer } from "./CommentsContainer";
