25f8e3251276d0330ec5b64bccdc3ca987f037a1
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-27 18:07:12 +0530

    create : file to store api endpoints

---
+++ src/services/api-urls.js
@@ -0,0 +1,12 @@
+export const API = {
+  BASE: "https://jarvis-share-backend.curiousguy.repl.co",
+  GET_ALL_USERS: "https://jarvis-share-backend.curiousguy.repl.co/users",
+  SIGNUP_USER: "https://jarvis-share-backend.curiousguy.repl.co/users/signup",
+  LOGIN_USER: "https://jarvis-share-backend.curiousguy.repl.co/users/login",
+  CREATE_NEW_POST: "https://jarvis-share-backend.curiousguy.repl.co/posts/new",
+  UPDATE_FOLLOW_FOLLOWERS:
+    "https://jarvis-share-backend.curiousguy.repl.co/users/follow",
+  GET_SINGLE_USER:
+    "https://jarvis-share-backend.curiousguy.repl.co/users/singleuser",
+  POST_COMMENT: "https://jarvis-share-backend.curiousguy.repl.co/posts/comment",
+};
