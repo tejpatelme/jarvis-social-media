9bb8b0f8049b7df9b663e556b183a05b022873fb
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-27 18:01:08 +0530

    create : posts page

---
+++ src/features/posts/Posts.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { CreatePost, PostCard } from "./components";
+import {
+  MobileTopNavbar,
+  LogoutModal,
+  FeedEmptyMessage,
+} from "../../components";
+import toast from "react-hot-toast";
+
+export default function Posts() {
+  const { posts, status, error } = useSelector((state) => state.posts);
+  const { currentUser } = useSelector((state) => state.auth);
+
+  const followingUsersPosts = posts.filter((post) => {
+    return (
+      currentUser?.following?.includes(post?.userId?._id) ||
+      post?.userId?._id === currentUser?._id
+    );
+  });
+
+  const sortedPosts = followingUsersPosts
+    ?.slice()
+    ?.sort((a, b) => b.postedOn.localeCompare(a.postedOn));
+
+  return (
+    <div className="md:max-w-2xl flex-grow p-4 md:p-5 space-y-3 mb-20">
+      <LogoutModal />
+      {error && toast.error(error)}
+      <MobileTopNavbar />
+      <CreatePost />
+      {status === "loading" && <p className="text-white">Loading...</p>}
+      <div className="space-y-3">
+        {sortedPosts?.map((post) => (
+          <PostCard key={post._id} post={post} />
+        ))}
+      </div>
+      {sortedPosts.length === 0 && status === "fulfilled" && (
+        <FeedEmptyMessage />
+      )}
+    </div>
+  );
+}
