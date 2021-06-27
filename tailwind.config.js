2f63a811bf3e6ef2d7afc21bdfbd54d508bdc21c
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-27 17:57:02 +0530

    configure : font-family, colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
+  mode: "jit",
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      Inter: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        gray: colors.gray,
+      },
+    },
   },
   variants: {},
   plugins: [],
