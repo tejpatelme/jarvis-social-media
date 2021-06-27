c8294ae7f47aea211f260005875ff6b9e1b19ae8
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-27 18:12:57 +0530

    create : routes for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+import { Routes, Route } from "react-router-dom";
+import { SideBar, BottomNavbar, PrivateRoute } from "./components";
+import { useDispatch, useSelector } from "react-redux";
+import { initializeCurrentUser } from "./features/auth/authSlice";
+import { Following, Profile, Search } from "./features/users";
+import { Posts, SinglePostPage } from "./features/posts";
+import { Login, Signup } from "./features/auth";
+import axios from "axios";
+import { loadPosts } from "./features/posts/postsSlice";
 
 function App() {
+  const { token } = useSelector((state) => state.auth);
+  const { users } = useSelector((state) => state.users);
+  const { status } = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const setupDefaultHeaders = () => {
+      if (token) {
+        return (axios.defaults.headers.common["Authorization"] = token);
+      }
+      delete axios.defaults.headers.common["Authorization"];
+    };
+
+    setupDefaultHeaders();
+  }, [token]);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(initializeCurrentUser());
+    }
+  }, [token, users]);
+
+  useEffect(() => {
+    if (token && status === "idle") {
+      dispatch(loadPosts());
+    }
+  }, [token]);
+
   return (
-    <div className="App">
-      <p>Hello Vite + React + Tailwindcss + Eslint</p>
+    <div className="App max-w-6xl m-auto">
+      <Toaster position="bottom-center" />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="*"
+          element={
+            <div className="block md:flex md:m-auto">
+              <SideBar />
+              <BottomNavbar />
+              <Routes>
+                <PrivateRoute path="/" element={<Posts />} />
+                <PrivateRoute
+                  path="/posts/:postId"
+                  element={<SinglePostPage />}
+                />
+                <PrivateRoute path="/profile/:userId" element={<Profile />} />
+                <PrivateRoute path="/following" element={<Following />} />
+                <PrivateRoute path="/search" element={<Search />} />
+              </Routes>
+            </div>
+          }
+        />
+      </Routes>
     </div>
   );
 }
