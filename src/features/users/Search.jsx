7852599987c286d138c54aad49f335fbd37f4871
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-29 16:21:06 +0530

    feat : exclude current authenticated user from search

--- src/features/users/Search.jsx
+++ src/features/users/Search.jsx
@@ -7,7 +7,7 @@ export default function Search() {
   const { currentUser } = useSelector((state) => state.auth);
   const { users } = useSelector((state) => state.users);
 
-  const filteredUsers = users.filter((user) => {
+  const filteredUsers = users?.filter((user) => {
     const name = `${user.firstName} ${user.lastName}`;
 
     return user._id === currentUser?._id
@@ -16,22 +16,26 @@ export default function Search() {
   });
 
   return (
-    <div className="md:max-w-2xl flex-grow p-4 md:p-5">
-      <div className="w-full mb-5">
-        <input
-          type="text"
-          placeholder="Search..."
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-          className="bg-gray-800 bg-opacity-60 rounded text-white w-full px-4 py-3 outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-40"
-        />
-      </div>
+    <>
+      {filteredUsers && (
+        <div className="md:max-w-2xl flex-grow p-4 md:p-5">
+          <div className="w-full mb-5">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              className="bg-gray-800 bg-opacity-60 rounded text-white w-full px-4 py-3 outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-40"
+            />
+          </div>
 
-      <div className="w-full space-y-3">
-        {filteredUsers?.map((user) => (
-          <UserCard key={user._id} user={user} />
-        ))}
-      </div>
-    </div>
+          <div className="w-full space-y-3">
+            {filteredUsers?.map((user) => (
+              <UserCard key={user._id} user={user} />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
