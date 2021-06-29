aca7167d278885a49fc08c3ea79f74209da0ce49
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-29 16:17:49 +0530

    re-export users features components

---
+++ src/features/users/components/index.jsx
@@ -0,0 +1,3 @@
+export { default as EditProfileButton } from "./EditProfileButton";
+export { default as ProfileDetails } from "./ProfileDetails";
+export { default as UsersPosts } from "./UsersPosts";
