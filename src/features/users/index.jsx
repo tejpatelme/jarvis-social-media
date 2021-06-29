943dd1f118ffc8a26a6cfdc41949d20908e4d55c
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-29 16:20:33 +0530

    re-export edit profile component

--- src/features/users/index.jsx
+++ src/features/users/index.jsx
@@ -1,3 +1,5 @@
 export { default as Profile } from "./Profile";
 export { default as Following } from "./Following";
+export { default as Followers } from "./Followers";
 export { default as Search } from "./Search";
+export { default as EditProfile } from "./EditProfile";
