3f58e31163eb437f9177aa52964cb2025222af2f
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-29 16:15:17 +0530

    refactor : remove comments

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -6,7 +6,6 @@ export default function Icon({ icon, size, color }) {
       className={`material-icons-round flex justify-center items-center ${
         color ? color : "text-gray-300"
       } cursor-pointer`}
-      // style={{ height: `${size}px`, width: `${size}px` }}
       style={{ fontSize: `${size}px` }}
     >
       {icon}
