f4e1928552d76de8c00f7c0a014bd37eaea5814b
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-30 07:05:09 +0530

    update : add extra styles prop

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function Icon({ icon, size, color }) {
+export default function Icon({ icon, size, color, extraStyles }) {
   return (
     <span
       className={`material-icons-round flex justify-center items-center ${
         color ? color : "text-gray-300"
-      } cursor-pointer`}
+      } cursor-pointer ${extraStyles ? extraStyles : ""}`}
       style={{ fontSize: `${size}px` }}
     >
       {icon}
