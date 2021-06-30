8ad6093309589eb65857b38a56e84ed53805e424
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-30 10:35:16 +0530

    update : disable redux dev tools in production mode

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -3,10 +3,13 @@ import postsSliceReducer from "../features/posts/postsSlice";
 import usersSliceReducer from "../features/users/usersSlice";
 import authSliceReducer from "../features/auth/authSlice";
 
+const enableDevTools = process.env.NODE_ENV === "development" ? true : false;
+
 export const store = configureStore({
   reducer: {
     posts: postsSliceReducer,
     users: usersSliceReducer,
     auth: authSliceReducer,
   },
+  devTools: enableDevTools,
 });
