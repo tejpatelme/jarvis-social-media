b2f28cd51701e1c589ca78762ae303cf9e4a97c3
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-09-23 14:55:48 +0530

    update : sort notifications by date

--- src/features/users/Notifications.jsx
+++ src/features/users/Notifications.jsx
@@ -6,14 +6,18 @@ import { NotificationCard } from "./components";
 export default function Notifications() {
   const { currentUser } = useSelector((state) => state.auth);
 
+  const sortedNotifications = currentUser.notifications
+    ?.slice()
+    ?.sort((a, b) => b.createdAt.localeCompare(a.createdAt));
+
   return (
     <>
       <Heading title="Notifications" />
       <div className="flex flex-col md:max-w-2xl p-4 md:p-5 space-y-2 flex-grow">
-        {currentUser && currentUser.notifications.length === 0 ? (
+        {currentUser && sortedNotifications.length === 0 ? (
           <FeedEmptyMessage text="You have no new notifications" />
         ) : (
-          currentUser.notifications.map((notification) => (
+          sortedNotifications.map((notification) => (
             <NotificationCard
               key={notification._id}
               notification={notification}
